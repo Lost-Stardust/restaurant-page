7c189cf04b8f4b9c9f69fbbc8de211912126dc62
Isa <Isaismail3000@gmail.com>
2023-07-02 21:44:10 +0300

    Give header a class and change text

--- src/load.js
+++ src/load.js
@@ -1,7 +1,8 @@
 const pageLoad = () => {
   const content = document.querySelector("#content");
   const heading = document.createElement("h1");
-  heading.textContent = "Ichiraku Ramen";
+  heading.classList.add("header");
+  heading.textContent = "Fries n' Thighs";
   content.appendChild(heading);
 };
 
