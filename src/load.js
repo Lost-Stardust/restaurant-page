1edaaf3fb85d15993e290fadd66f03c2d327d227
Isa <Isaismail3000@gmail.com>
2023-07-02 23:11:47 +0300

    Create and append h2 to #content

--- src/load.js
+++ src/load.js
@@ -10,6 +10,10 @@ const pageLoad = () => {
   desc.textContent =
     " The first thing people notice about French fries is the way it looks. There is lots of sizes of it. It could be long and thick for some restaurant, and there’s a small and thin size, but everything is cooked precisely and delicious. The smell of French fries is getting your attention. A really good French fries when it’s cooked fresh, some of them is putting a spicy seasoning to make it smell like spicy.";
   content.appendChild(desc);
+
+  const heading2 = document.createElement("h2");
+  heading2.textContent = "Popular dishes";
+  content.appendChild(heading2);
 };
 
 export { pageLoad };
