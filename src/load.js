81255dc722a56c9c02db751d680939c56e89ae1f
Isa <Isaismail3000@gmail.com>
2023-08-03 02:27:11 +0300

    Create and append header to body

--- src/load.js
+++ src/load.js
@@ -1,3 +1,12 @@
-const pageLoad = () => {};
+const pageLoad = () => {
+  const body = document.querySelector("body");
+
+  const content = document.querySelector("#content");
+  console.log(content);
+
+  //   create and append header to body
+  const header = document.createElement("header");
+  body.insertBefore(header, content);
+};
 
 export { pageLoad };
