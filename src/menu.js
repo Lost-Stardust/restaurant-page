5b59b169e801870a22bc0a28cc6fd70760fd1dbf
Isa <Isaismail3000@gmail.com>
2023-08-15 11:17:24 +0300

    Create array that contains menu item divs, add classes to them

--- src/menu.js
+++ src/menu.js
@@ -2,5 +2,14 @@ const loadMenu = () => {
   // empty out content
   const content = document.querySelector("#content");
   content.replaceChildren();
+
+  // create divs for menu items
+  const items = [];
+  let n = 0;
+  for (let i = 0; i < 8; i++) {
+    items[i] = document.createElement("div");
+    items[i].className = "item" + ++n;
+    console.log(items);
+  }
 };
 export { loadMenu };
