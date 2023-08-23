f19b7a78b415c53bb332794749f65dc1c37bdf8c
Isa <Isaismail3000@gmail.com>
2023-08-23 20:07:21 +0300

    Clear content before any operations, remove commented header code

--- src/home.js
+++ src/home.js
@@ -6,47 +6,7 @@ const loadHome = () => {
   console.log(content);
 
   content.classList.remove("menu");
-
-  // //   create and append header to body
-  // const header = document.createElement("header");
-  // body.insertBefore(header, content);
-
-  // // logo container
-  // const logoContainer = document.createElement("div");
-  // logoContainer.className = "logo-container";
-  // header.appendChild(logoContainer);
-
-  // // Icon
-  // const myIcon = new Image();
-  // myIcon.src = Icon;
-  // myIcon.className = "icon";
-  // logoContainer.appendChild(myIcon);
-
-  // // logo
-  // let logo = document.createElement("h2");
-  // logo.textContent = "Sizzle joint";
-  // logo.className = "logo";
-  // logoContainer.appendChild(logo);
-
-  // // tab container
-  // const tabContainer = document.createElement("div");
-  // tabContainer.className = "tab-container";
-  // header.appendChild(tabContainer);
-
-  // // tabs
-  // const tab1 = document.createElement("p");
-  // const tab2 = document.createElement("p");
-  // const tab3 = document.createElement("p");
-  // tabContainer.appendChild(tab1);
-  // tabContainer.appendChild(tab2);
-  // tabContainer.appendChild(tab3);
-  // tab1.textContent = "Home";
-  // tab2.textContent = "Menu";
-  // tab3.textContent = "Contact";
-  // let tabs = [tab1, tab2, tab3];
-  // for (let i = 0; i < tabs.length; i++) {
-  //   tabs[i].className = "tab";
-  // }
+  content.replaceChildren();
 
   // image in content
   const img = document.createElement("img");
