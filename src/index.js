145972c9ce8026c22d6870610c612ff15163aa93
Isa <Isaismail3000@gmail.com>
2023-08-24 10:34:18 +0300

    Import loadContact module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { loadHome } from "./home.js";
 import { loadMenu } from "./menu.js";
+import { loadContact } from "./contact.js";
 import Icon from "./burger.png";
 
 const body = document.querySelector("body");
