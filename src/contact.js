43902da08777eed2eecd302217e5bb232169df52
Isa <Isaismail3000@gmail.com>
2023-08-24 15:43:08 +0300

    Add class to content, create and append heading

--- src/contact.js
+++ src/contact.js
@@ -2,5 +2,12 @@ const loadContact = () => {
   // empty out content
   const content = document.querySelector("#content");
   content.replaceChildren();
+
+  content.className = "contact";
+
+  const heading = document.createElement("h1");
+  heading.className = "contact-heading";
+  heading.textContent = "Contact us";
+  content.appendChild(heading);
 };
 export { loadContact };
