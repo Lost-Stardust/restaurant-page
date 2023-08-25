db65e31f759f8e2ec7a279adf7d6890ad24399a5
Isa <Isaismail3000@gmail.com>
2023-08-25 21:54:06 +0300

    Create div that contains contact information

--- src/contact.js
+++ src/contact.js
@@ -9,5 +9,17 @@ const loadContact = () => {
   heading.className = "contact-heading";
   heading.textContent = "Contact us";
   content.appendChild(heading);
+
+  const info = document.createElement("div");
+  info.className = "info";
+  content.appendChild(info);
+
+  const num = document.createElement("p");
+  num.textContent = "Phone number: 725-235-5574";
+  const email = document.createElement("p");
+  email.textContent = "Email: SizzleJoint@gmail.com";
+
+  info.appendChild(num);
+  info.appendChild(email);
 };
 export { loadContact };
